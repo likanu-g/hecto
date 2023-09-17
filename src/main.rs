264c92ae9f013c90ab415b8f8f9e1df6132abeb2
likanug <liukang515@gmail.com>
2023-09-17 12:59:45 +0800

    拆分main.ts和Editor重构项目，增加Clippy检测代码风格

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,7 @@
-use std::io::{self, stdout};
-
-use termion::{event::Key, input::TermRead, raw::IntoRawMode};
-
+#![warn(clippy::all, clippy::pedantic)]
+mod editor;
+use editor::Editor;
 fn main() {
-    // 下划线变量名会让编译器忽略这个变量没有使用的警告
-    let _stdout = stdout().into_raw_mode().unwrap();
-
-    for key in io::stdin().keys() {
-        match key {
-            Ok(key) => match key {
-                Key::Char(c) => {
-                    if c.is_control() {
-                        println!("{:?}\r", c as u8);
-                    } else {
-                        println!("{:?} ({})\r", c as u8, c);
-                    }
-                }
-                Key::Ctrl('d') => break,
-                _ => println!("{:?}\r", key),
-            },
-            Err(err) => die(err),
-        }
-    }
-}
-
-fn die(e: std::io::Error) {
-    panic!("{}", e);
+    let editor = Editor::default();
+    editor.run();
 }
