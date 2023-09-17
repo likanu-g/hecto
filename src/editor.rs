9772c1d44fd65bea9b109783914db4f4fb2d7f47
likanug <liukang515@gmail.com>
2023-09-17 14:38:27 +0800

    增加Terminal重构项目

--- src/editor.rs
+++ src/editor.rs
@@ -1,15 +1,14 @@
-use std::io::{self, stdout, Write};
+use crate::Terminal;
+
 use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
 
 pub struct Editor {
     should_quit: bool,
+    terminal: Terminal,
 }
 
 impl Editor {
     pub fn run(&mut self) {
-        let _stdout = stdout().into_raw_mode().unwrap();
         loop {
             if let Err(error) = self.process_screen() {
                 die(&error)
@@ -24,11 +23,14 @@ impl Editor {
     }
 
     pub fn default() -> Self {
-        Self { should_quit: false }
+        Self {
+            should_quit: false,
+            terminal: Terminal::default().expect("Failed to initialize terminal"),
+        }
     }
 
     fn process_keypress(&mut self) -> Result<(), std::io::Error> {
-        let pressed_key = read_key()?;
+        let pressed_key = Terminal::read_key()?;
         match pressed_key {
             Key::Ctrl('d') => self.should_quit = true,
             _ => (),
@@ -37,34 +39,25 @@ impl Editor {
     }
 
     fn process_screen(&self) -> Result<(), std::io::Error> {
-        //print!("\x1b[2j"); x1b 是十进制27 键盘的Escape按键
-        //等同于上面的语句，termion已经实现了清屏的功能
-        print!("{} {}", termion::clear::All, termion::cursor::Goto(1, 1)); //清屏后光标移动到终端(1,1)的坐标
+        Terminal::clear_screen();
+        Terminal::cursor_position(0, 0);
         if self.should_quit {
             println!("Good bye. \r")
         } else {
             self.draw_rows();
-            print!("{}", termion::cursor::Goto(1, 1)); //清屏后光标移动到终端(1,1)的坐标
+            Terminal::cursor_position(0, 0); //清屏后光标移动到终端(1,1)的坐标
         }
-        io::stdout().flush()
+        Terminal::flush()
     }
 
     fn draw_rows(&self) {
-        for _ in 0..24 {
+        for _ in 0..self.terminal.size().height {
             println!("~\r");
         }
     }
 }
 
-fn read_key() -> Result<Key, std::io::Error> {
-    loop {
-        if let Some(key) = io::stdin().lock().keys().next() {
-            return key;
-        }
-    }
-}
-
 fn die(e: &std::io::Error) {
-    print!("{}", termion::clear::All);
+    Terminal::clear_screen();
     panic!("{}", e);
 }
