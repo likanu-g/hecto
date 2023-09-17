a644ebbf781dbc95355d7eeb7278ac1eb04ff310
likanug <liukang515@gmail.com>
2023-09-17 11:49:07 +0800

    增加ctrl+d退出和使用模式匹配重构代码

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,29 @@ fn main() {
     let _stdout = stdout().into_raw_mode().unwrap();
 
     for b in io::stdin().bytes() {
-        let c = b.unwrap() as char;
-        println!("{}", c);
-        if c == 'q' {
-            break;
+        match b {
+            Ok(b) => {
+                let c = b as char;
+                if c.is_control() {
+                    println!("{:?} \r", b)
+                } else {
+                    println!("{:?} ({})\r", b, c);
+                }
+                if b == to_ctrl_byte('d') {
+                    break;
+                }
+            }
+            Err(err) => die(err),
         }
     }
+}
+
+fn to_ctrl_byte(c: char) -> u8 {
+    let byte = c as u8;
+    println!("b2:{:#b}", byte & 0b0001_1111);
+    byte & 0b0001_1111
+}
+
+fn die(e: std::io::Error) {
+    panic!("{}", e);
 }
