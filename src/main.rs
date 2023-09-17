a5be498b1aad0f049902a60cb2b8eae99c597e63
likanug <liukang515@gmail.com>
2023-09-17 12:09:48 +0800

    使用termion::event::Key替代原先输入的byte重构代码

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,27 @@
-use std::io::{self, stdout, Read};
+use std::io::{self, stdout};
 
-use termion::raw::IntoRawMode;
+use termion::{event::Key, input::TermRead, raw::IntoRawMode};
 
 fn main() {
     // 下划线变量名会让编译器忽略这个变量没有使用的警告
     let _stdout = stdout().into_raw_mode().unwrap();
 
-    for b in io::stdin().bytes() {
-        match b {
-            Ok(b) => {
-                let c = b as char;
-                if c.is_control() {
-                    println!("{:?} \r", b)
-                } else {
-                    println!("{:?} ({})\r", b, c);
+    for key in io::stdin().keys() {
+        match key {
+            Ok(key) => match key {
+                Key::Char(c) => {
+                    if c.is_control() {
+                        println!("{:?}\r", c as u8);
+                    } else {
+                        println!("{:?} ({})\r", c as u8, c);
+                    }
                 }
-                if b == to_ctrl_byte('d') {
-                    break;
-                }
-            }
+                Key::Ctrl('d') => break,
+                _ => println!("{:?}\r", key),
+            },
             Err(err) => die(err),
         }
     }
-}
-
-fn to_ctrl_byte(c: char) -> u8 {
-    let byte = c as u8;
-    println!("b2:{:#b}", byte & 0b0001_1111);
-    byte & 0b0001_1111
 }
 
 fn die(e: std::io::Error) {
